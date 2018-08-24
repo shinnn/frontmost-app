6439419dc6fe52468278fbf2dc40bfaf601a1d3a
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-08-24 10:40:58 +0900

    expose async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -5,32 +5,30 @@ if (process.platform === 'darwin') {
 
 	const pify = require('pify');
 
-	const option = {timeout: 2000};
 	const binPath = require.resolve('./frontmost-app');
-
+	const option = {timeout: 2000};
 	const promisifiedExecFile = pify(execFile);
 
-	module.exports = function frontmostApp(...args) {
+	module.exports = async function frontmostApp(...args) {
 		const argLen = args.length;
 
 		if (argLen !== 0) {
-			return Promise.reject(new RangeError(`Expected no arguments, but got ${argLen} argument${
+			throw new RangeError(`Expected no arguments, but got ${argLen} argument${
 				argLen === 1 ? '' : 's'
-			}.`));
+			}.`);
 		}
 
-		return promisifiedExecFile(binPath, option).then(stdout => { // eslint-disable-line promise/prefer-await-to-then
-			const [localizedName, bundleId, bundlePath, executablePath, isLaunched, pid] = stdout.split('\x07');
+		const properites = (await promisifiedExecFile(binPath, option)).split('\x07');
+		const [localizedName, bundleId, bundlePath, executablePath, isLaunched, pid] = properites;
 
-			return {
-				localizedName,
-				bundleId,
-				bundlePath,
-				executablePath,
-				isLaunched: !!isLaunched,
-				pid: parseInt(pid, 10)
-			};
-		});
+		return {
+			localizedName,
+			bundleId,
+			bundlePath,
+			executablePath,
+			isLaunched: !!isLaunched,
+			pid: parseInt(pid, 10)
+		};
 	};
 
 	Object.defineProperty(module.exports, 'supported', {
@@ -40,11 +38,11 @@ if (process.platform === 'darwin') {
 } else {
 	const platformName = require('platform-name');
 
-	module.exports = function frontmostApp() {
+	module.exports = async function frontmostApp() {
 		const error = new Error(`frontmost-app only supports macOS, but the current platform is ${platformName()}.`);
 		error.code = 'ERR_UNSUPPORTED_PLATFORM';
 
-		return Promise.reject(error);
+		throw error;
 	};
 
 	Object.defineProperty(module.exports, 'supported', {
